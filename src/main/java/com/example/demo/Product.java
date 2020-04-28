a9110a4214b51e9e18987284ab64e5404fd414c2
JJongMani <3827925@naver.com>
2020-04-28 11:14:33 +0900

    dd

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
+import javax.persistence.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Entity
+public class Product {
+    @Id @GeneratedValue
+    Long id;
+    String name;
+    int stock;
+
+    @PostPersist
+    public void eventPublish(){
+        ProductChanged productChanged = new ProductChanged();
+        productChanged.setProductId(this.getId());
+        productChanged.setProductName(this.getName());
+        productChanged.setProductStock(this.getStock());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+
+        try {
+            json = objectMapper.writeValueAsString(productChanged);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON format exception", e);
+        }
+        System.out.println(json);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
